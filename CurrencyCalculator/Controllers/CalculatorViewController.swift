c4c2af2ed2dadc3f58f8400851b7fb9519d45e7f
anjalinarang <aknarang03@gmail.com>
2024-09-08 10:52:09 -0400

    Added change conversion factor functionality

--- CurrencyCalculator/Controllers/CalculatorViewController.swift
+++ CurrencyCalculator/Controllers/CalculatorViewController.swift
@@ -12,6 +12,8 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var dollarAmount: UITextField!
     @IBOutlet weak var euroAmount: UITextField!
     
+    @IBOutlet weak var conversionFactor: UITextField!
+    
     let calculator = Calculator()
     
     override func viewDidLoad() {
@@ -20,12 +22,17 @@ class CalculatorViewController: UIViewController {
         dollarAmount.text = "0.00"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        conversionFactor.text = String (calculator.getConversionFactor())
+    }
+    
     @IBAction func convertToEuro(_ sender: UIButton) {
         var euros = 0.00
         if let dollar = Double (dollarAmount.text!) {
             euros = calculator.dollarToEuro(dollar: dollar)
         }
         euroAmount.text = String (format: "%.2f", euros)
+        view.endEditing(true)
     }
     
     @IBAction func convertToDollar(_ sender: UIButton) {
@@ -34,6 +41,18 @@ class CalculatorViewController: UIViewController {
             dollars = calculator.euroToDollar(euro: euro)
         }
         dollarAmount.text = String (format: "%.2f", dollars)
+        view.endEditing(true)
+    }
+    
+    @IBAction func changeConversionFactor(_ sender: UIButton) {
+        if let rate = Double (conversionFactor.text!) {
+            calculator.setConversionFactor(rate: rate)
+        } 
+        else { // e.g. user entered nothing
+            calculator.setConversionFactor(rate: 0.00)
+            conversionFactor.text = String ("0.00")
+        }
+        view.endEditing(true)
     }
     
 }
