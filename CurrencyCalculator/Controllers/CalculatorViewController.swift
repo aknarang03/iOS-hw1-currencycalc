7011fd79efc4730bac755cfbf2bf4ba8960ca302
anjalinarang <aknarang03@gmail.com>
2024-09-11 12:05:01 -0400

    based on keyboard collapse, conversion factor text will always reflect the value in the calculator, even when enter is not pressed

--- CurrencyCalculator/Controllers/CalculatorViewController.swift
+++ CurrencyCalculator/Controllers/CalculatorViewController.swift
@@ -20,12 +20,23 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
         euroAmount.text = "0.00"
         dollarAmount.text = "0.00"
+        
+        // add this controller as an observer of the keyboard hide notification
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide),
+                                               name: UIResponder.keyboardDidHideNotification,
+                                               object: nil)
+    }
+    
+    // this is so that the conversion factor text will reset back to what it was if user did not press Enter button
+    @objc func keyboardDidHide(notification: Notification) {
+        conversionFactor.text = String (calculator.getConversionFactor())
     }
     
     override func viewWillAppear(_ animated: Bool) {
         conversionFactor.text = String (calculator.getConversionFactor())
     }
-    
+        
     @IBAction func convertToEuro(_ sender: UIButton) {
         var euros = 0.00
         if let dollar = Double (dollarAmount.text!) {
